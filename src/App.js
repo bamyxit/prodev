9c1d2edb9ea611d8090a7203bb35808ac5ec4ee1
Joseph <joseph.abayomi@pbgdigital.co.uk>
2021-01-18 16:56:23 +0100

    First comit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./scss/App.scss";
+import Home from "./pages/homePage/Home";
+const App = () => {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+};
+
+export default App;
